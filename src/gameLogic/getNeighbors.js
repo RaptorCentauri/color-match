b7a2bf0392b343610449ecc7cec6bf1ad7833a91
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-07-20 00:52:44 -0500

    getNeighbors now returns a map

--- src/gameLogic/getNeighbors.js
+++ src/gameLogic/getNeighbors.js
@@ -7,63 +7,51 @@ let colors = [
 Object.defineProperty(Array.prototype, 'getNeighbors', {
         value(obj) {
             try {
-                if (typeof(obj) != 'object') {
-                   throw 'getNeighbors expects an object!'  
-                }
+                if (typeof(obj) != 'object') throw 'getNeighbors expects an object!'  
 
-                if (typeof (obj.row) != 'number') {
-                    throw 'getNeighbors expects an obj.row to be a number!'
-                }
+                if (typeof (obj.row) != 'number') throw 'getNeighbors expects an obj.row to be a number!'
 
-                if (typeof (obj.col) != 'number') {
-                    throw 'getNeighbors expects an obj.col to be a number!'
-                }
+                if (typeof (obj.col) != 'number') throw 'getNeighbors expects an obj.col to be a number!'
+                
+                if(!obj.hasOwnProperty('row')) throw 'getNeighbors expects a row property!'
 
-                if(!obj.hasOwnProperty('row')){
-                    throw 'getNeighbors expects a row property!'
-                }
+                if (!obj.hasOwnProperty('col')) throw 'getNeighbors expects a col property!'
 
-                if (!obj.hasOwnProperty('col')) {
-                    throw 'getNeighbors expects a col property!'
-                }
+                if (Object.keys(obj).length != 2) throw 'getNeighbors expects an object with only 2 keys: row & col!'
 
-                // if (obj.keys()) {
-                //     throw 'getNeighbors expects a col property!'
-                // }
-
-
-
-            const neighbors = {};
+            const neighbors = new Map();
 
             if ((obj.row - 1) >= 0) {
                 const north = this[obj.row - 1][obj.col];
-                neighbors.north = north;
+                neighbors.set({row: obj.row-1, col: obj.col}, north)
             }
 
             if ((obj.row + 1) <= (this.length - 1)) {
                 const south = this[obj.row + 1][obj.col];
-                neighbors.south = south;
+                neighbors.set({row:obj.row+1, col:obj.col}, south)
             }
 
             if ((obj.col + 1) <= (this[obj.row].length - 1)) {
                 const east = this[obj.row][obj.col + 1];
-                neighbors.east = east;
+                neighbors.set({row:obj.row, col:obj.col+1}, east)
             }
 
             if ((obj.col - 1) >= 0) {
                 const west = this[obj.row][obj.col - 1];
-                neighbors.west = west;
+                neighbors.set({row:obj.row, col:obj.col-1}, west)
             }
 
             return neighbors
 
             } catch (error) {
-                console.log(error);
+                console.error(`Error: ${error}`);
             }
     }
 });
 
-let moo = colors.getNeighbors({row:1, col: 2});
+let moo = colors.getNeighbors({row:1, col: 1});
 // let moo = colors.getNeighbors("1");
 
-console.log('moo', moo);
+console.log(moo.entries());
+
+
