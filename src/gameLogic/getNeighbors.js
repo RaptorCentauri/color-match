564a9e2ac7380f98095931e5458672f10977fca5
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-07-19 21:44:30 -0500

    switch getNeighbors to a Array.prototype

--- src/gameLogic/getNeighbors.js
+++ src/gameLogic/getNeighbors.js
@@ -1,100 +1,69 @@
-const myArr = [
-['a','b', 'c', 'd', 'e'],
-['f', 'g', 'h', 'i', 'j'],
-['k', 'l', 'm', 'n', 'o'],
-['p', 'q', 'r', 's', 't'],
-['u', 'v', 'w', 'x', 'y']
-]
-
 let colors = [
 ['r','b', 'y'],
 ['r', 'y', 'y'],
 ['b', 'r', 'y']
 ]
 
-//create matrix
-let foo = Array.from({ length: 5 }, () => Array.from({ length: 5 }, () => Math.floor(Math.random() * 4)));
+Object.defineProperty(Array.prototype, 'getNeighbors', {
+        value(obj) {
+            try {
+                if (typeof(obj) != 'object') {
+                   throw 'getNeighbors expects an object!'  
+                }
+
+                if (typeof (obj.row) != 'number') {
+                    throw 'getNeighbors expects an obj.row to be a number!'
+                }
+
+                if (typeof (obj.col) != 'number') {
+                    throw 'getNeighbors expects an obj.col to be a number!'
+                }
+
+                if(!obj.hasOwnProperty('row')){
+                    throw 'getNeighbors expects a row property!'
+                }
+
+                if (!obj.hasOwnProperty('col')) {
+                    throw 'getNeighbors expects a col property!'
+                }
+
+                // if (obj.keys()) {
+                //     throw 'getNeighbors expects a col property!'
+                // }
 
 
 
-createMatrix = (rows, col) => {
-    let foo = Array.from({ length: rows }, () => Array.from({ length: col }, () => Math.floor(Math.random() * 4)));
-    return foo;
-}
+            const neighbors = {};
 
-// console.log('foo', foo);
-// console.log('myArr', myArr);
+            if ((obj.row - 1) >= 0) {
+                const north = this[obj.row - 1][obj.col];
+                neighbors.north = north;
+            }
 
-// let bar = createMatrix(5, 5);
-// console.log(bar);
+            if ((obj.row + 1) <= (this.length - 1)) {
+                const south = this[obj.row + 1][obj.col];
+                neighbors.south = south;
+            }
 
+            if ((obj.col + 1) <= (this[obj.row].length - 1)) {
+                const east = this[obj.row][obj.col + 1];
+                neighbors.east = east;
+            }
 
-// Array.prototype.getNeighbors =
+            if ((obj.col - 1) >= 0) {
+                const west = this[obj.row][obj.col - 1];
+                neighbors.west = west;
+            }
 
-// Array.prototype.getNeighbors  = (arr, row, col) => {
-//     const neighbors = {};
-//     const home = arr[row][col];
+            return neighbors
 
-//     if ((row-1) >= 0) {
-//         const north = arr[row-1][col];
-//         neighbors.north = north;
-//     }
+            } catch (error) {
+                console.log(error);
+            }
+    }
+});
 
-//     if ((row + 1) <= (arr.length-1)) {
-//         const south = arr[row + 1][col];
-//         neighbors.south = south;
-//     }
+let moo = colors.getNeighbors({row:1, col: 2});
+// let moo = colors.getNeighbors("1");
 
-//     if ((col + 1) <= (arr[row].length-1)) {
-//         const east = arr[row][col+1];
-//         neighbors.east = east;
-//     }
-
-//     if ((col - 1) >= 0) {
-//         const west = arr[row][col - 1];
-//         neighbors.west = west;
-//     }
-
-//     console.log(neighbors);
-// }
-
-// getRowsAmount = (arr) => {
-//     const rows = (arr.length-1);
-//     return rows;
-// }
-
-Array.prototype.getNeighbors = (row, col) => {
-    const neighbors = {};
-    // const home = arr[row][col];
-
-    console.log('this', this);
-    
-
-    // if ((row - 1) >= 0) {
-    //     const north = this[row - 1][col];
-    //     neighbors.north = north;
-    // }
-
-    // if ((row + 1) <= (this.length - 1)) {
-    //     const south = this[row + 1][col];
-    //     neighbors.south = south;
-    // }
-
-    // if ((col + 1) <= (this[row].length - 1)) {
-    //     const east = this[row][col + 1];
-    //     neighbors.east = east;
-    // }
-
-    // if ((col - 1) >= 0) {
-    //     const west = this[row][col - 1];
-    //     neighbors.west = west;
-    // }
-
-    // console.log(neighbors);
-}
-
-let bar = createMatrix(5, 5);
-
-// console.log('bar', bar);
-
-bar.getNeighbors(1,1);
+console.log('moo', moo);
