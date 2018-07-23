de527427b5570393a833eb958ba64dc60ca6859d
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-07-23 15:44:06 -0500

    cleaned up shiftMatrixDown.js

--- src/gameLogic/shiftMatrixDown.js
+++ src/gameLogic/shiftMatrixDown.js
@@ -1,63 +1,31 @@
-import { isNull } from "util";
-
 /*
 shiftMatrixDown will move any all indeces down vertically as long as there is an empty spot
 */
 
-
 const shiftMatrixDown = Object.defineProperty(Array.prototype, 'shiftMatrixDown', {
     value() {
         try {
-            //check if bottom row has missing elemnt;
             for (let i = 0; i < this.length; i++) {
-                
                 let checkRowVal = (this.length-1)-i;
                 let newRowVal = checkRowVal-1;
 
-                // console.log(`================ itteration ${i} ================`);
                 let checkRow = this[checkRowVal];
                 let newRow = this[newRowVal];
 
-
                 if (checkRowVal > 0) {
-                    
-                
 
-                for (let k = 0; k < checkRow.length; k++) {
-                    // console.log(`############## SUB-itteration ${k} ##############`);
+                    for (let k = 0; k < checkRow.length; k++) {
+                        let checkPos = checkRow[k]
 
-                    let checkPos = checkRow[k]
-
-                    if (checkPos === undefined) {
-                        // console.log('crv', checkRowVal);
-                        
-                        // console.log('nrv', newRowVal);
-                        // console.log('k', k);
-                        
-                        
-                        let newValue = this[newRowVal][k];
-                        this[checkRowVal][k] = newValue;
-                        // console.log('alpha', newRow[k]);
-                        delete newRow[k];
-                        // console.log('beta', newRow[k]);
-
-                        
+                        if (checkPos === undefined) {
+                            let newValue = this[newRowVal][k];
+                            this[checkRowVal][k] = newValue;
+                            delete newRow[k];
+                        }
                     }
                 }
 
             }
-
-            }
-
-   
-
-            // this[2 row][1 col] = this[1 row][1 col];
-            // this[1 row][1 col] = this[0 row][1 col];
-            // delete this[0 row][1 col];
-
-            // this[2][1] = this[1][1];
-            // this[1][1] = this[0][1];
-            // delete this[0][1];
       
         } catch (e) {
             throw e
@@ -66,19 +34,3 @@ const shiftMatrixDown = Object.defineProperty(Array.prototype, 'shiftMatrixDown'
 });
 
 export default shiftMatrixDown
-
-
-let testMatrix = [[0,1,3],[4,5,6],[7,8,9]];
-
-console.log('before', testMatrix);
-
-
-delete testMatrix[2][1];
-// delete testMatrix[2][2];
-
-console.log('middle', testMatrix);
-
-
-testMatrix.shiftMatrixDown();
-
-console.log('after', testMatrix);
