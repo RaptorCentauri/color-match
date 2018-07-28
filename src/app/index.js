dcf2467faa1c29289ae9ca7aef706843f94d2466
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-07-28 12:48:57 -0400

    can now change board size

--- src/app/index.js
+++ src/app/index.js
@@ -20,12 +20,6 @@ class App extends React.Component {
   
 
     fullPlay = (i) => {
-
-        let N = this.state.board.getNeighbors(i)
-        console.log('N', N.size);
-        
-
-
         this.state.board.destroyChainfromID(i)
         this.setState({board: this.state.board })
 
@@ -35,32 +29,8 @@ class App extends React.Component {
         let genRandNum = () => Math.floor(Math.random()* 4)
         this.state.board.fillEmptyValues(genRandNum)
         this.setState({board: this.state.board })
-
     }
 
-    handlePlay = (i) => {
-        this.state.board.destroyChainfromID(i)
-        this.setState({board: this.state.board })
-
-    }
-
-    handleSquareClick = (i) => {
-        this.state.board.dropDown();
-        this.setState({board: this.state.board })
-    }
-
-    handleSquareClickDELETE = () =>{
-        let genRandNum = () => Math.floor(Math.random()* 4)
-        this.state.board.fillEmptyValues(genRandNum)
-        this.setState({board: this.state.board })
-    }
-
-    setStyleFromValue = (i) =>{
-        if (i === 0) return 'blue'
-        if (i === 1) return 'red'
-        if (i === 2) return 'green'
-        if (i === 3) return 'yellow'
-    }
 
     componentDidMount = () => {
         console.log(this.state.board);
@@ -69,7 +39,7 @@ class App extends React.Component {
     render() {
         return (
             <div className='App'>
-                <div className='board-frame'>
+                <div className={`board-frame-${this.state.board.size}`}>
                     {this.state.boardItterator.map(i => <Square key={i} 
                         value={this.state.board.getValueOfId(i)}
                         clickHandler={this.fullPlay.bind(this, i)}
--- src/gameLogic/MatrixClass/methods/destroyChainfromID.js
+++ src/gameLogic/MatrixClass/methods/destroyChainfromID.js
@@ -1,15 +1,11 @@
 const destroyChainfromID = function(id){
     let chain = this.getChainfromID(id);
 
-    console.log('chain', chain.size);
-    
     if (chain.size > 1) {
         chain.forEach(c => {
             this.deleteValueOfId(c)
         })
     }
-
-
 }
 
 export default destroyChainfromID
