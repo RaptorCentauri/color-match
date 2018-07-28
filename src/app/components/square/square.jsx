63fac38a2eab7ef7bbe8e27a0bff4704886ed376
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-07-28 13:05:31 -0400

    added scoring

--- src/app/components/square/square.jsx
+++ src/app/components/square/square.jsx
@@ -7,32 +7,5 @@ const Square = (props) =>
 <div onClick={props.clickHandler} className={`square-style square-color-${props.value}`}  >
 </div>
 
-
-// class Square extends React.Component {
-//     constructor(props){
-//         super(props)
-//         console.log('Sq', this.props.value);
-//     }
-
-//     setColorByValue = (i) =>{
-//         if (i === 0) return 'blue'
-//         if (i === 1) return 'red'
-//         if (i === 2) return 'green'
-//         if (i === 3) return 'yellow'
-
-//     }
-
-//     render(){
-//         return(
-//     <div onClick={this.props.clickHandler} className={`square-style ${this.setColorByValue(this.props.value)}`}  >
-
-
-//         </div>
-//         )
-//     }
-
-
-// }
-
 export default Square;
 
--- src/app/index.js
+++ src/app/index.js
@@ -13,18 +13,47 @@ class App extends React.Component {
 
         this.state = {
             board: gameBoard,
-            boardItterator: gameBoard.keysAsArray
+            boardItterator: gameBoard.keysAsArray,
+            score: 0
         }
 
     }
   
 
+    getScore = (value, count) => {
+        let moveScore = value * count;
+        // console.log('v', value);
+        // console.log('c', count);
+
+        
+        // console.log(moveScore);
+        this.setState({score: this.state.score + moveScore})
+    }
+
     fullPlay = (i) => {
+
+        let value = this.state.board.getValueOfId(i);
+        // console.log('INV', value);
+
+        let S = this.state.board.getChainfromID(i)
+        if (S.size > 1) {
+            let value = this.state.board.getValueOfId(i);
+            console.log('INV', value);
+            this.getScore(value, S.size)
+        }
+
         this.state.board.destroyChainfromID(i)
         this.setState({board: this.state.board })
 
+
+   
+
+
+
+
         this.state.board.dropDown();
         this.setState({board: this.state.board })
+
 
         let genRandNum = () => Math.floor(Math.random()* 4)
         this.state.board.fillEmptyValues(genRandNum)
@@ -45,6 +74,7 @@ class App extends React.Component {
                         clickHandler={this.fullPlay.bind(this, i)}
                     />)}
                 </div>
+                Score:{this.state.score}
             </div>
         );
     }
