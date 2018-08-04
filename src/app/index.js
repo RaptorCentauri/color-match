98d16cc7ecea1ec72700bf8cb8e4cf55cdb34ab2
RaptorCentauri <dave.leo.shilander@icloud.com>
2018-08-04 12:01:55 -0500

    styling

--- src/app/index.js
+++ src/app/index.js
@@ -35,37 +35,6 @@ class App extends React.Component {
         return moveScore;
     }
 
-
-
-    setGameOver = () => {
-        // for (let i = 1; i <= this.state.board.size ; i++) {
-        //     if (this.state.board.westEdge.has(i)) {
-        //         this.state.board.setValueOfid(i, 2);                        
-        //     }
-        //     else if (this.state.board.eastEdge.has(i)){
-        //         this.state.board.setValueOfid(i, 1);
-        //     }
-        //     else if(i % 2 === 0){
-        //         this.state.board.setValueOfid(i, 1);
-        //     }
-        //     else{
-        //         this.state.board.setValueOfid(i, 2);
-        //     } 
-        // }
-        
-        for (let i = 1; i <= this.state.board.size ; i++) {
-            if (i%2===0) {
-                this.state.board.setValueOfid(i, 2);
-            }
-            else{
-                this.state.board.setValueOfid(i, 1);
-            }
-        }
-
-
-         this.setState({board: this.state.board})
-    }
-
     playGame = (board, i) => {
         let round ={
             board: board,
@@ -94,8 +63,6 @@ class App extends React.Component {
         this.setState({score: this.state.score + round.score})
     }
 
-
-    
     checkForGameOver = (board, i) =>{
         do {
             if (board.hasEquivalentNeighbors(i)){
@@ -107,13 +74,6 @@ class App extends React.Component {
         return true;
     }
 
-
-    GOBUTTON = () => {
-        let go = this.checkForGameOver(this.state.board, 1);
-        console.log('GAME IS OVER', go);
-        
-    }
-
     componentDidUpdate = () =>{
         
         let newLevel = this.levelUp(this.state.score);
@@ -122,19 +82,11 @@ class App extends React.Component {
             this.setState({level: newLevel})
         }
 
-        //check Game over
         let isG = this.checkForGameOver(this.state.board, 1);
-        console.log('GameOVER:', isG);
-        
 
         // if (isG) {
-        //     console.log('game is over');
+        //     this.setState({gameOver: true})
         // }
-        // else{
-        //     console.log('game is not over');
-        // }
-
-
 
     }
 
@@ -142,7 +94,7 @@ class App extends React.Component {
     render() {
         return (
             <div className='App'>
-                <div className={`board-frame-${this.state.board.size}`}>
+                <div className={`board-frame board-size-${this.state.board.size}`}>
                     {this.state.boardItterator.map(i => <Square key={i} 
                         value={this.state.board.getValueOfId(i)}
                         id={i}
@@ -150,10 +102,8 @@ class App extends React.Component {
                         clickHandler={this.handleSquareClick.bind(this, i)}
                     />)}
                 </div>
-                <Score score={this.state.score} level={this.state.level}/>
-                {this.state.gameOver.toString()}
-                <button onClick={this.setGameOver}>SET GAME OVER</button>
-                <button onClick={this.GOBUTTON}>GO CHECK</button>  
+                {/* <Score score={this.state.score} level={this.state.level}/> */}
+                {/* {this.state.gameOver.toString()} */}
             </div>
         );
     }
