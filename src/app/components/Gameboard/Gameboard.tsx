9770d894101a9dc121127b4c673e0f88f6a6da83
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-06-11 23:24:27 -0500

    context hook

--- src/app/components/Gameboard/Gameboard.tsx
+++ src/app/components/Gameboard/Gameboard.tsx
@@ -3,19 +3,21 @@ import './Gameboard.scss';
 import Square from '../Square/Square';
 import  {AppContext} from '../../contextAndProvider';
 
-const Gameboard = () =>
-    <AppContext.Consumer>
-        {(context) => (
-          <div className={`board-size-${Math.sqrt(context.state.board.size)}`}>
-              {context.state.boardItterator.map(i =>
-               <Square key={i}
-                  id={i}
-                  value={context.state.board.getValueOfID(i)}
-                  />
-                )}
-          </div>
-        )}
-    </AppContext.Consumer>
+const Gameboard = () =>{
+
+const context = React.useContext(AppContext);
+
+return(
+    <div className={`board-size-${Math.sqrt(context.state.board.size)}`}>
+        {context.state.boardItterator.map(i =>
+         <Square key={i}
+            id={i}
+            value={context.state.board.getValueOfID(i)}
+            />
+          )}
+    </div>
+  )
+}
 
 
 export default Gameboard;
