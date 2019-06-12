e65bb8ed71c43053df8f2b0e7aec11a3d89bbbac
RaptorCentauri <dave.leo.shilander@icloud.com>
2019-06-11 23:21:47 -0500

    using context hook

--- src/app/components/Gameover/Gameover.tsx
+++ src/app/components/Gameover/Gameover.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import './Gameover.scss';
 import { AppContext } from '../../contextAndProvider';
 
-const Gameover = (props) =>
-<div className={`Gameover-style`}>
-    GAME OVER
-    <AppContext.Consumer>
-        {(context) => (
-                <button className={`play-again-button`} onClick={context.playAgain}>Play Again?</button>
-        )}
-    </AppContext.Consumer>
-</div>
+const Gameover = (props) => {
 
+const context = React.useContext(AppContext);
+
+    return(
+      <div className={`Gameover-style`}>
+          GAME OVER
+          <button className={`play-again-button`} onClick={context.playAgain}>Play Again?</button>
+      </div>
+    )
+}
 export default Gameover;
