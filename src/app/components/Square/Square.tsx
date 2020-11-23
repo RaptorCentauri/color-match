698b36cd98e953434fe4c359986d260c7933f726
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-11-23 12:26:56 -0500

    switched to hooks

--- src/app/components/Square/Square.tsx
+++ src/app/components/Square/Square.tsx
@@ -2,44 +2,22 @@ import * as React from 'react';
 import "./Square.scss";
 import { AppContext } from '../../contextAndProvider';
 
-class Square extends React.Component{
-  state: { value: any; animateClass: string; };
-  props: any;
-    constructor(props: any){
-        super(props);
-        this.state= {
-            value: this.props.value,
-            animateClass: 'initial'
-        }
-    }
+const Square = ({value,id}) => {
+    const [animateClass, setAnimateClass] = React.useState('initial')
 
-    static getDerivedStateFromProps(nextProps: { value: any; }, _prevState: any){
-            if (nextProps.value === null) {
-                return {animateClass: 'destroy'}
-            }
+    React.useEffect(()=>{
+        value ? setAnimateClass('drop') : setAnimateClass('destroy')
+    })
 
-            if (nextProps.value != null) {
-                return {value: nextProps.value, animateClass: 'drop'}
-            }
-        else return null;
-    }
-
-    render(){
-        return(
-          <AppContext.Consumer>
+    return (
+        <AppContext.Consumer>
             {(context) => (
-                <colored-square onClick={context.squareClick.bind(this, this.props.id)}
-                    class={`id-${this.props.id} square-style square-color-${this.state.value} square-animate-${this.state.animateClass}`}>
-                </colored-square> 
+                <colored-square onClick={()=>context.squareClick(id)} 
+                    class={`id-${id} square-style square-color-${value} square-animate-${animateClass}`}>
+                </colored-square>
             )}
         </AppContext.Consumer>
-
-
-        )
-    }
-
+    )
 }
-
-
 
 export default Square;
