ffc44d771f9f62603a1a2b69f7d2469fdbbde5bc
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-11-23 14:11:47 -0500

    imported React

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 declare namespace JSX {
     interface IntrinsicElements {
         'colored-square': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
@@ -11,14 +13,5 @@ declare namespace JSX {
         'play-again-btn': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         'board-frame': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         'color-match': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-
-
-
-
-        // 'clock-face': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        // 'clock-number': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        // 'sub-num': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        // 'clockframe': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-
     }
 }
