e4d15ecd87609577405c81fa035c3bc8a3386d80
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-11-23 12:41:41 -0500

    added namespace

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -1,6 +1,7 @@
 declare namespace JSX {
     interface IntrinsicElements {
         'colored-square': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'game-board': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         // 'clock-hand': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         // 'clock-face': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         // 'clock-number': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
