f5efe7a2e15ad0a1863d65d0ee35d19886f90937
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-11-23 13:14:27 -0500

    added namespace for titlebar

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -2,7 +2,14 @@ declare namespace JSX {
     interface IntrinsicElements {
         'colored-square': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         'game-board': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        // 'clock-hand': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'title-bar': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'title-name': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'score-box': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'current-score': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+        'current-level': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+
+
+
         // 'clock-face': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         // 'clock-number': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
         // 'sub-num': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
