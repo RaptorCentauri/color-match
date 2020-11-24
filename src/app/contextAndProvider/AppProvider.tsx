292b76d8164ac81fe94813d01d1050a3518e35da
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-11-24 00:01:41 -0500

    moved functions outside of provider

--- src/app/contextAndProvider/AppProvider.tsx
+++ src/app/contextAndProvider/AppProvider.tsx
@@ -2,11 +2,25 @@ import * as React from 'react';
 import Matrix from 'matrix-map';
 import * as gameLogic from '../gameLogic/index';
 import AppContext from './AppContext';
-import ScoreContext from '../contextAndProvider/scoreContext'
+
+
+// const changeBoard = (board, state) => {
+//     const updatedBoard = [...state.board];
+
+//     return {...state, board: updatedBoard}
+// }
+
+// const UPDATE_BOARD = 'UPDATE_BOARD';
+// const boardReducer = (state, action) => {
+//     switch(action.type){
+//         case UPDATE_BOARD:
+//             return
+//     }
+// }
 
 
 const AppProvider = props => {
-    const numberOfSquares = 36;
+    const numberOfSquares = 25;
     const numberOfValues = 7;
     let gameBoard = new Matrix(numberOfSquares);
     let genRandNum = () => Math.floor((Math.random() * (numberOfValues - 1)) + 1);
@@ -18,7 +32,6 @@ const AppProvider = props => {
     const [level, setLevel] = React.useState(1);
     const [gameOver, setGameOver] = React.useState(false);
 
-
     const nextLevelCheck = () => {
         let nextLevel = gameLogic.levelUp(score);
 
@@ -27,69 +40,48 @@ const AppProvider = props => {
         }
     } 
 
-    const gameOverCheck = () => {
+    const gameOverCheck = () => {        
         let isGameOver = gameLogic.checkForGameOver(board, 1);
+        setGameOver(isGameOver)
+    }
 
-        if (gameOver != isGameOver) {
-            setGameOver(isGameOver)
-        }
+    const playAgain = () => {
+        let reset = gameLogic.resetBoard(board, numberOfValues);
+        setBoard(reset.board)
+        setLevel(1)
+        setScore(0)
+        setGameOver(false)
+    }
+
+    const squareClick = (i) => {
+        let round = gameLogic.playGame(board, i);
+        setBoard(round.board);
+
+        let drop = gameLogic.dropSquares(board, numberOfValues);
+        setBoard(drop.board)
+
+        setScore(score + round.score)
     }
 
     React.useEffect(()=>{                
         nextLevelCheck();
-        gameOverCheck()
+        gameOverCheck();        
     })
 
-    // React.useEffect(()=>{
-    //    const timer = setTimeout(
-    //         () => {
-    //             console.log('in timeout');
-    //         }, 5000
-    //     );
-    //     return ()=> clearTimeout(timer)
-    // })
-
-
-
     return (
-        <AppContext.Provider value={{
-             score:score,
-             level: level,
-             board: board,
-             boardItterator: boardItterator,
-             
-            playAgain: () => {
-                let reset = gameLogic.resetBoard(board, numberOfValues);
-                setBoard(reset.board)
-                setLevel(1)
-                setScore(0)
-                setGameOver(false)
-
-            },
-
-            squareClick: (i) => {
-                let round = gameLogic.playGame(board, i);
-                
-                setBoard(round.board);
-
-                let drop = gameLogic.dropSquares(board, numberOfValues);
-
-                setTimeout(() => {
-
-                    setBoard(drop.board);
-                },3000)
-                
-
-                setScore(score+round.score)
-            },
-
-        }}>
+        <AppContext.Provider 
+            value={{
+                score:score,
+                level: level,
+                board: board,
+                boardItterator: boardItterator,
+                gameOver: gameOver,
+                playAgain: playAgain,
+                squareClick: squareClick
+            }}>
             {props.children}
         </AppContext.Provider>
     )
-
-
 }
 
- 
 export default AppProvider;
