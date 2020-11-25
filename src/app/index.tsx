c7ba2afcd43eea334ac397ea96eae32c7d0edd97
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-11-25 16:27:36 -0500

    removed unuswd classes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,20 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import './index.scss'
-import {AppContext, AppProvider} from './contextAndProvider'
+import {AppProvider} from './contextAndProvider'
 import {Titlebar, Gameboard, Gameover,Boardframe} from './components'
 
 const App = (props) => {
-
-        return (
-            <AppProvider>
-            <color-match class='App'>
+    return (
+        <AppProvider>
+            <color-match>
                 <Titlebar />
                     <Boardframe>
                         <Gameboard />
                     </Boardframe>
             </color-match>
-            </AppProvider>
-        );
+        </AppProvider>
+    );
 }
 
 
