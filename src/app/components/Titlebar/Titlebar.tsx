58844ff6e08f34d80550cd9949050c155195e39c
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-11-25 13:12:47 -0500

    using custom elements, removed class names. simplified layout

--- src/app/components/Titlebar/Titlebar.tsx
+++ src/app/components/Titlebar/Titlebar.tsx
@@ -5,10 +5,10 @@ import { AppContext } from '../../contextAndProvider';
 const Titlebar = (props) =>
     <AppContext.Consumer>
         {(context) => (
-            <title-bar class={`titlebar-style-new`}>
-                    <current-level class={`titleGroup`}>Level: {context.level}</current-level>
-                    <title-name class={`title`}>Color Match</title-name>
-                    <current-score class={`titleGroup`}>Score: {context.score}</current-score>
+            <title-bar>
+                    <current-level>Level: {context.level}</current-level>
+                    <title-name>Color Match</title-name>
+                    <current-score>Score: {context.score}</current-score>
             </title-bar>
         )}
     </AppContext.Consumer>
