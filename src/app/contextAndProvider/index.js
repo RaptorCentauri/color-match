2571835bed41ef51b11524e928e02d3b53af9b73
RaptorCentauri <dave.leo.shilander@icloud.com>
2020-11-25 20:31:40 -0500

    removed js files

--- src/app/contextAndProvider/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AppContext_1 = require("./AppContext");
-exports.AppContext = AppContext_1["default"];
-var AppProvider_1 = require("./AppProvider");
-exports.AppProvider = AppProvider_1["default"];
--- src/app/gameLogic/globals.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var numberOfSquares = 16;
-exports.numberOfSquares = numberOfSquares;
-var numberOfValues = 10;
-exports.numberOfValues = numberOfValues;
-var randomValues = function () { return Math.floor((Math.random() * (numberOfValues - 1)) + 1); };
-exports.randomValues = randomValues;
